f85aa5f218d9fb0a8e12a24d139aa28dfa3d4a4b
Rachael Thompson <rachael.thompson@speakwrite.com>
2018-10-03 19:23:00 -0500

    HelloWorld/create new project Quiz

---
+++ Quiz1/Quiz1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Quiz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = ""; 
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello QUIZ 1!");
+            Console.WriteLine("What is your name?");
+            Console.ReadLine(name);
+            Console.WriteLine("My name is ", name);
+        }
+    }
+}

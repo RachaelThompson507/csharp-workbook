af0836a7b68375415037d5d2349a23f5e6285ddd
Rachael Thompson <rachael.thompson@speakwrite.com>
2018-10-03 20:54:00 -0500

    setting up textGame project.cs

---
+++ textGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace textGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("Welcome to the cavern of secrets!");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        }
+    }
+}

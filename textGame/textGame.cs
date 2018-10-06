ca42bdebec8988f769b5b8b53893a4017bb4284f
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-10-06 17:43:37 -0500

    changed stick to lowercase

--- textGame/textGame.cs
+++ textGame/textGame.cs
@@ -7,23 +7,26 @@ namespace textGame
     {
         static void Main(string[] args)
         {
-            int Stick = 0;
+            //game start
+            int stick = 0;
             Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             Console.WriteLine("Welcome to the cavern of secrets!");
             Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             Thread.Sleep(3000);
+            //do you take stick
             Console.WriteLine("You enter a dark cavern out of curiosity. It is dark and you can only make out a small stick on the floor.");
             Console.WriteLine("Do you take it [Y/N]");
             String ch1 = Console.ReadLine();
-
+            //if take stick statement
             if(ch1 == "y" || ch1 == "Y" || ch1 == "Yes" || ch1 == "yes") {
                 Console.WriteLine("You have taken the stick!");
                 Thread.Sleep(2000);
-                Stick = 1;
+                stick = 1;
             }else {
-                Console.WriteLine("You did not take the stick!");
-                Stick = 0;
+                Console.WriteLine("You did not take the stick.");
+                stick = 0;
             }
+            //approach spider
             Console.WriteLine("As you proceed further into the cave, you see a small glowing object");
             Console.WriteLine("Do you approach the object? [Y/N]");
             String ch2 = Console.ReadLine();

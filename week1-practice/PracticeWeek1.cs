b1d7a944a493dded8cf401334115efce260ca878
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-10-07 10:33:20 -0500

    adding integers practice complete

--- week1-practice/PracticeWeek1.cs
+++ week1-practice/PracticeWeek1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace week1_practice
 {
@@ -6,7 +7,20 @@ namespace week1_practice
     {
         static void Main(string[] args)
         {
+            //hello world
             Console.WriteLine("Hello World!");
+            Thread.Sleep(2000);
+            // 1-add 2 numbers and display result
+            Console.WriteLine("What 2 numbers would you like to add?");
+            Console.WriteLine("Please enter number 1");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter number 2");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The answer is...");
+            Console.WriteLine("{0}+{1}={2}", a, b, a+b);
+            Thread.Sleep(2000);
+            //2- converting yard to inches
+
         }
     }
 }

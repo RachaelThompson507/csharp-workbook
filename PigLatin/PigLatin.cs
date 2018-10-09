314c5ec1a64097b6fb7ef2c502ab18d2ba69c1a9
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-10-08 20:33:46 -0500

    updated the main method to call the TranslateWord method

--- PigLatin/PigLatin.cs
+++ PigLatin/PigLatin.cs
@@ -1,20 +1,31 @@
 ﻿using System;
-
+using System.Threading;
 namespace PigLatin
 {
     class Program
     {
         public static void Main()
         {
-            // your call code goes here
-
+            //Declare variable string
+            String word = " ";
+            //Welcome user to application
+            Console.WriteLine("Converting Words to Pig Latin");
+            Thread.Sleep(2000);
+            //User enters a word
+            Console.WriteLine("Please enter a Word: ");
+            word = Console.ReadLine();
+            //TranslateWord method is called and passed the word to translate.
+            TranslateWord(word);
+            //Translated word is displayed to the user.
+            //?-how to pass the translated word back to the user
+            Console.WriteLine(word);
             // leave this command at the end so your program does not close automatically
             Console.ReadLine();
         }
 
         public static string TranslateWord(string word)
         {
-            // your code goes here
+            
             return word;
         }
     }

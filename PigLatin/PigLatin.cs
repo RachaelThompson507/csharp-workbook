6ca8c1b6998bb1e4f054764a6b4f046c1564a243
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-10-08 19:48:49 -0500

    PigLatin minor comment update

--- PigLatin/PigLatin.cs
+++ PigLatin/PigLatin.cs
@@ -6,12 +6,12 @@ namespace PigLatin
     {
         public static void Main()
         {
-            // your code goes here
+            // your call code goes here
 
             // leave this command at the end so your program does not close automatically
             Console.ReadLine();
         }
-        
+
         public static string TranslateWord(string word)
         {
             // your code goes here

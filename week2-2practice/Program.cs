2d04bc4a4d97333e4b4f303816560a2c28ee2a8d
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-10-10 20:25:55 -0500

    added notes

--- week2-2practice/Program.cs
+++ week2-2practice/Program.cs
@@ -9,23 +9,23 @@ namespace week2_2practice
             //ask user for a number
             //if # is an A, print out 'you made an A'
             //an A is 90 to 100
-            Console.WriteLine("Please enter a number.");
-            int grade = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("Please enter a number.");
+            // int grade = Convert.ToInt32(Console.ReadLine());
 
-            if(grade >=90)
-            {
-                Console.WriteLine("You have an A!");
-            } else if (grade >=80)
-            {
-                Console.WriteLine("You have a B.");
-            }else if (grade >=70)
-            {
-                Console.WriteLine("You have a C.");
-            } else
-            {
-                Console.WriteLine("You did not pass.");
-            }
-            Console.WriteLine("End Program");
+            // if(grade >=90)
+            // {
+            //     Console.WriteLine("You have an A!");
+            // } else if (grade >=80)
+            // {
+            //     Console.WriteLine("You have a B.");
+            // }else if (grade >=70)
+            // {
+            //     Console.WriteLine("You have a C.");
+            // } else
+            // {
+            //     Console.WriteLine("You did not pass.");
+            // }
+            // Console.WriteLine("End Program");
 
             //bool logic example
             String name = null;

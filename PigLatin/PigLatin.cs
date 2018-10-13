bf53417e12931482c4027761e648abe79c520d0e
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-10-13 15:09:25 -0500

    first char y, first char vowel, first char no vowels complete

--- PigLatin/PigLatin.cs
+++ PigLatin/PigLatin.cs
@@ -1,64 +1,66 @@
-﻿using System;
-using System.Threading;
-namespace PigLatin
-{
-    class Program
+﻿    using System;
+    using System.Threading;
+    using System.Linq;
+    namespace PigLatin
     {
-        public static void Main()
+        class Program
         {
-            //Welcome user to application
-            Console.WriteLine("Converting Words to Pig Latin");
-            Thread.Sleep(2000);
-
-            //User enters a word
-            Console.WriteLine("Please enter a Word: ");
-
-            //enter a string var for translation
-            String word = (Console.ReadLine());
-            //convert word to lowercase
-            word = word.ToLower();
-            //TranslateWord method is called and passed the word to translate.
-            String translatedWord = TranslateWord(word);
-
-            //Translated word is displayed to the user.
-            //?-how to pass the translated word back to the user
-            Console.WriteLine(translatedWord);
-
-            // leave this command at the end so your program does not close automatically
-            Console.ReadLine();
-        }
-
-            /*Logic:
-            If StartsWith Y - Y/N
-                T- +yay to end of word
-            Else if starts w/ Check vowels w/Y
-                T- +yay to end of word
-            ELSE F- check for first vowel (including Y) and
-                move everything before 1st vowel to the end of the word
-                then +ay
-             */
-            // return word; (will need to be added to each if)
-        public static string TranslateWord(string word)
-        {
-            //Declaring Variables
-             string beginY = "y";
-             string addYay = "yay";
-             string addAy = "ay";
-             
-            if (word.StartsWith(beginY))
+            public static void Main()
             {
-                word = string.Concat(word,addYay);
-                return word;
-            } else if ()
+                //Welcome user to application
+                Console.WriteLine("Converting Words to Pig Latin");
+                Thread.Sleep(2000);
+
+                //User enters a word
+                Console.WriteLine("Please enter a Word: ");
+
+                //enter a string var for translation
+                String word = (Console.ReadLine().ToLower());
+
+                //TranslateWord method is called and passed the word to translate.
+                String translatedWord = TranslateWord(word);
+
+                //Translated word is displayed to the user.
+                //?-how to pass the translated word back to the user
+                Console.WriteLine(translatedWord);
+
+                // leave this command at the end so your program does not close automatically
+                Console.ReadLine();
+            }
+
+                /*Logic:
+                If StartsWith Y - Y/N
+                    T- +yay to end of word
+                Else if starts w/ Check vowels w/Y
+                    T- +yay to end of word
+                ELSE F- check for first vowel (including Y) and
+                    move everything before 1st vowel to the end of the word
+                    then +ay
+                */
+            public static string TranslateWord(string word)
             {
-                word = string.Concat(word,addYay);
+                //Declaring Variables
+                string addYay = "yay";
+                string addAy = "ay";
+                string vowel = "aeiou";
+                string firstChar = word[0].ToString();
+
+                if (firstChar == "y")
+                {
+                    word = string.Concat(word,addYay);
+                    return word;
+
+                } else if (vowel.Contains(firstChar))
+                {
+                    word = string.Concat(word,addYay);
+                    return word;
+                }
+                else if (!vowel.Contains(firstChar))
+                {
+                    word = string.Concat(word, addAy);
+                   return word;
+                }
                 return word;
             }
-            // else ()
-            // {
-
-            // }
-            return word;
         }
     }
-}

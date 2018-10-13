5c9648e6d00c27162f18829f818510bc90871faa
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-10-13 12:22:55 -0500

    stuck on check vowel at beginning of word

--- PigLatin/PigLatin.cs
+++ PigLatin/PigLatin.cs
@@ -9,22 +9,55 @@ namespace PigLatin
             //Welcome user to application
             Console.WriteLine("Converting Words to Pig Latin");
             Thread.Sleep(2000);
+
             //User enters a word
             Console.WriteLine("Please enter a Word: ");
+
             //enter a string var for translation
-            String word = Convert.ToString(Console.ReadLine());
+            String word = (Console.ReadLine());
+            //convert word to lowercase
+            word = word.ToLower();
             //TranslateWord method is called and passed the word to translate.
-            TranslateWord(word);
+            String translatedWord = TranslateWord(word);
+
             //Translated word is displayed to the user.
             //?-how to pass the translated word back to the user
-            Console.WriteLine(word);
+            Console.WriteLine(translatedWord);
+
             // leave this command at the end so your program does not close automatically
             Console.ReadLine();
         }
 
+            /*Logic:
+            If StartsWith Y - Y/N
+                T- +yay to end of word
+            Else if starts w/ Check vowels w/Y
+                T- +yay to end of word
+            ELSE F- check for first vowel (including Y) and
+                move everything before 1st vowel to the end of the word
+                then +ay
+             */
+            // return word; (will need to be added to each if)
         public static string TranslateWord(string word)
         {
-            
+            //Declaring Variables
+             string beginY = "y";
+             string addYay = "yay";
+             string addAy = "ay";
+             
+            if (word.StartsWith(beginY))
+            {
+                word = string.Concat(word,addYay);
+                return word;
+            } else if ()
+            {
+                word = string.Concat(word,addYay);
+                return word;
+            }
+            // else ()
+            // {
+
+            // }
             return word;
         }
     }

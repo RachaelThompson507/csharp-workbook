b9cfa5776039d6398d0cb06c0991e1e3cf0244ad
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-10-15 19:33:11 -0500

    added array notes / dir

---
+++ week3-practice/arrayPractice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace arrayPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            String[] names = new String [4];
+            names[0] = "James";
+            names[1] = "Mike";
+            names[2] = "Adam";
+            names[3] = "Jane";
+
+            String pets = { "Rocky", "Walter", "Tibs", "Bailey"};
+
+            String [] cities;
+            cities = new String[]{"Austin", "Dallas", "Houston"};
+
+            Console.WriteLine("Names {0}, {1}, {2}, {3}.", names);
+
+        }
+    }
+}

87889f11a9afdcb03eb5e1f409b1cba0e402f6be
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-10-15 19:53:32 -0500

    end practice for monday

--- week3-practice/listPractice/Program.cs
+++ week3-practice/listPractice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace listPractice
 {
@@ -6,7 +7,14 @@ namespace listPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            List<String> names = new List<String>();
+            names.Add("John");
+            names.Add("Mike");
+            names.Add("Mark");
+
+            Console.WriteLine(" The list has {0} elements ", names.Count);
+            Console.WriteLine(" The first name is {0} ", names[0]);
         }
     }
 }

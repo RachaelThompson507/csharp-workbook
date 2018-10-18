5b04777bf5fed48ec46a08d52792ac4714eb78e0
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-10-18 18:58:06 -0500

    FizzBuzzComplete

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -6,7 +6,38 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+            FizzBuzz:
+            Count from the number 1
+            Any number divisible by 3 = Fizz
+            Any number divisible by 5 = Buzz
+            Numbers that are divisible by both = FizzBuzz
+             */
+             Console.WriteLine("FizzBuzz");
+             Console.WriteLine("Enter a number...");
+             int numEntered = Convert.ToInt32(Console.ReadLine());
+             //fizzBuzz(numEntered);
+             //Console.WriteLine(fizzBuzz(numEntered));
+             fizzBuzz(numEntered);
+
+        }
+        public static void fizzBuzz(int numEntered)
+        {
+            int i = 0;
+            for (i=0; i<=numEntered; i++){
+                bool fizz = i % 3 == 0;
+                bool buzz = i % 5 == 0;
+                if(fizz && buzz){
+                   Console.WriteLine( "FizzBuzz" );
+                } else if (fizz){
+                    Console.WriteLine("Fizz");
+                }else if (buzz){
+                    Console.WriteLine( "Buzz");
+                } else {
+                    string i1 = i.ToString();
+                    Console.WriteLine( i1);
+                }
+            } //return "Could not find the number";
         }
     }
 }

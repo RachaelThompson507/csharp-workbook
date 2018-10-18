2a35556c15027d95be62478ad8f35db187103310
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-10-17 20:14:39 -0500

    practice loops note

---
+++ week3-practice/loops/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int sum = 0;
+            // for ( int i =0; i<6; i++)
+            // {
+            //     sum += i;
+            // }
+            // Console.WriteLine("Sum is " + sum);
+
+            // int i = 1;
+            // while (i<6){
+            //     sum+=i;
+            //     i++;
+            // }
+
+            //max number in array
+            // int [] x = {9, 13, 4, 12 ,3};
+            // int largest = x[0];
+
+            // for ( int i =0; i < x.Length; i++ ){
+            //      if(largest < x[i]){
+            //          largest = x[i];
+            //      }
+            // } Console.WriteLine(largest);
+            //find mean/median/mode/sum for various arrays
+
+            int largest = mynums[0];
+            foreach( int currentNum in mynums){
+                if(largest < currentNum){
+                    largest = currentNum;
+                }
+            }
+
+        }
+    }
+}

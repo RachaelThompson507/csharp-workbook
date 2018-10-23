b40ee783b4cd9e37146b82126420383cd82179ef
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-10-22 20:09:32 -0500

    program 1 start

--- Checkpoint1/Checkpoint1.cs
+++ Checkpoint1/Checkpoint1.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 
-namespace Checkpoint1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Checkpoint1 {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Hello World!");
+            countMod3();
+        }
+        //program to count how many numbers between 1 & 100 are %3
+        //display the count by calling fx in main w/ writeline
+        public static void countMod3 () {
+            for (int i = 1; i <= 100; i ++) {
+                if (i%3 == 0) {
+                    //Console.WriteLine(i);
+                }
+            }
         }
     }
 }

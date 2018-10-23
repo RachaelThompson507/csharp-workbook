a4cdacd316d8b1c2720a5eee9a4efcb21ee80ea3
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-10-22 21:01:37 -0500

    completed first checkpoint question regarding number of times %3 occurs in the loop between 1- 100

--- Checkpoint1/Checkpoint1.cs
+++ Checkpoint1/Checkpoint1.cs
@@ -1,19 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Checkpoint1 {
     class Program {
         static void Main (string[] args) {
-            Console.WriteLine ("Hello World!");
-            countMod3();
+            Console.WriteLine ("-----------------------------------------");
+            Console.WriteLine ("Hello, Welcome to Rachael's Checkpoint 1!");
+            Console.WriteLine ("-----------------------------------------");
+            countMod3 ();
         }
         //program to count how many numbers between 1 & 100 are %3
         //display the count by calling fx in main w/ writeline
         public static void countMod3 () {
-            for (int i = 1; i <= 100; i ++) {
-                if (i%3 == 0) {
-                    //Console.WriteLine(i);
+            List<int> countMod3 = new List<int> ();
+            int count = 0;
+            for (int i = 1; i <= 100; i++) {
+                if (i % 3 == 0) {
+                    countMod3.Add (count++);
                 }
             }
+            Console.WriteLine ("The number of times a number between 1 and 100 was divisible by 3 \nwithout a remainder was {0}.", countMod3.Count);
         }
     }
 }

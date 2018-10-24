1f8590efc250bbe5f3d6084aed5b7af45bd40cbe
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-10-23 22:24:02 -0500

    need to add an try-catch to #2 and handle exception

--- Checkpoint1/Checkpoint1.cs
+++ Checkpoint1/Checkpoint1.cs
@@ -9,9 +9,10 @@ namespace Checkpoint1 {
             Console.WriteLine ("Hello, Welcome to Rachael's Checkpoint 1!");
             Console.WriteLine ("-----------------------------------------");
             countMod3 ();
+            sumNum();
         }
-        //program to count how many numbers between 1 & 100 are %3
-        //display the count by calling fx in main w/ writeline
+        /*1- Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder.
+        Display the count on the console. */
         public static void countMod3 () {
             List<int> countMod3 = new List<int> ();
             int count = 0;
@@ -22,5 +23,20 @@ namespace Checkpoint1 {
             }
             Console.WriteLine ("The number of times a number between 1 and 100 was divisible by 3 \nwithout a remainder was {0}.", countMod3.Count);
         }
+        /* 2- Write a program and continuously ask the user to enter a number or "ok" to exit.
+        Calculate the sum of all the previously entered numbers and display it on the console.*/
+        public static void sumNum () {
+            while (Console.ReadLine() != "ok") {
+                Console.WriteLine("Please enter a number or type 'ok' to exit.");
+                int num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter a number or type 'ok' to exit.");
+                int num2 = int.Parse(Console.ReadLine());
+                Console.WriteLine(" {0} + {1} = {2} ", num1, num2, num1+num2);
+            }
+        }
+        /*Write a program and ask the user to enter a number.
+        Compute the factorial of the number and print it on the console.
+        For example, if the user enters 5, the program should
+        calculate 5 x 4 x 3 x 2 x 1 and display it as 5! = 120. */
     }
 }

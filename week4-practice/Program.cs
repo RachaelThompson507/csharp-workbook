a225d7caa4520705a6ebd4d276014a0420a2b411
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-10-24 18:55:37 -0500

    Stacks vs List

---
+++ week4-practice/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace week4_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            List<string> names = new List<string> {"Bob", "Johnny", "Jack", "Mark", "Joy", "Rachael"};
+            foreach(string name in names){
+                Console.WriteLine(name);
+            }
+        }
+    }
+}

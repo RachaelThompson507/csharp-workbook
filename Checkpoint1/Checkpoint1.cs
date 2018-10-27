0a12122cb9948fcb45d7e3dc522452ce68911ee7
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-10-27 11:21:23 -0500

    number 5 complete, need exceptions and tests

--- Checkpoint1/Checkpoint1.cs
+++ Checkpoint1/Checkpoint1.cs
@@ -23,6 +23,10 @@ namespace Checkpoint1 {
             Console.WriteLine ("             Guessing Game               ");
             Console.WriteLine ("-----------------------------------------");
             guessGame ();
+            Console.WriteLine ("-----------------------------------------");
+            Console.WriteLine ("             Maximum Number              ");
+            Console.WriteLine ("-----------------------------------------");
+            maxNum ();
         }
         /*1- Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder.
         Display the count on the console. */
@@ -85,6 +89,27 @@ namespace Checkpoint1 {
                     Console.WriteLine ("You lost.");
                 }
             }
+        }
+        /*  5- Write a program and ask the user to enter a series of numbers separated by comma.
+        Find the maximum of the numbers and display it on the console.
+        For example, if the user enters “5, 3, 8, 1, 4", the program should display 8.
+        */
+        public static void maxNum () {
+            Console.Write ("Enter comma separated numbers: ");
+            var input = Console.ReadLine ();
+
+            var numbers = input.Split (',');
+
+            // Assume the first number is the max
+            var max = Convert.ToInt32 (numbers[0]);
+
+            foreach (var str in numbers) {
+                var number = Convert.ToInt32 (str);
+                if (number > max)
+                    max = number;
+            }
+
+            Console.WriteLine ("Max is " + max);
         }
     }
 }

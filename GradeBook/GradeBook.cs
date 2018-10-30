2babb075eeff1cfeead0e0c9520a4fbc6e79d607
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-10-30 12:47:16 -0500

    add data to dictionary

--- GradeBook/GradeBook.cs
+++ GradeBook/GradeBook.cs
@@ -21,8 +21,8 @@ namespace GradeBook {
         public static void dataEntry () {
             // I started using Stacks- which worked but I could not get to work with a dictionary.
             // I cannot get the lists to work either-- any ideas?
-            List <string> studentNames = new List <string> {};
-            List <string> studentGrades = new List <string> {} ;
+            String studentNames = (" ");
+            List <int> studentGrades = new List <int> {} ;
             //Dictionary <List<string>, List<string>> gradeBook = new Dictionary<List<string>, List<string>>();
 
             while (true) {
@@ -32,11 +32,12 @@ namespace GradeBook {
                 // if user is entering data do --- else if break ---else goto top
                 if (inputYN == "Y") {
                     Console.WriteLine ("Enter students name in format 'last name , first name'.");
-                    studentNames.Insert(0,(Console.ReadLine ().ToUpper()));
-                    //interpolation is really cool!
-                    Console.WriteLine ($"Enter grades for {studentNames[0]}\nEach grade must be seperated by a coma.\nExample: (88,95)");
-                    studentGrades.Insert(0,(Console.ReadLine ()));
-                    //tried entering into dictionary here - didn't work threw exception
+                    studentNames = (Console.ReadLine ().ToUpper());
+
+                    Console.WriteLine ($"Enter grades for {studentNames}\nEach grade must be a number.\nYou may enter as many grades as you would like.");
+                    Console.WriteLine ($"To Exit entering grades for {studentNames}, type done.");
+                    //to loop and add grades to a list - exit when done;
+                
                 } else if (inputYN == "N") {
                     break;
 

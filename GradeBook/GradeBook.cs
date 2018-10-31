ec2c73dbbdb957e51e11bf6b6d7d7c21f2600863
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-10-30 22:52:40 -0500

    gradebook to dictionary completed for dataEntry

--- GradeBook/GradeBook.cs
+++ GradeBook/GradeBook.cs
@@ -6,10 +6,11 @@ namespace GradeBook {
     class Program {
         static void Main (string[] args) {
             Console.WriteLine ("Grade Book an Application by GradePro");
-        /*
-        The data entry method takes in the data to be analyzed.
-        */
+            /*
+            The data entry method takes in the data to be analyzed.
+            */
             dataEntry ();
+
         }
         /*
         Contract:
@@ -21,38 +22,41 @@ namespace GradeBook {
         public static void dataEntry () {
             // I started using Stacks- which worked but I could not get to work with a dictionary.
             // I cannot get the lists to work either-- any ideas?
-            String studentNames = (" ");
-            List <int> studentGrades = new List <int> {} ;
-            //Dictionary <List<string>, List<string>> gradeBook = new Dictionary<List<string>, List<string>>();
+            String studentName = " ";
+            string studentGrades = " ";
+            String studentInputYN = " ";
+            Dictionary<String, String> gradeBook = new Dictionary<String, String> ();
 
             while (true) {
                 //if the user does not enter a Y || N they will be directed back here to try again
                 Beginning : Console.WriteLine ("Select Y to enter data, N to return calculations.");
-                string inputYN = Console.ReadLine ().ToUpper ();
+                studentInputYN = Console.ReadLine ().ToUpper ();
                 // if user is entering data do --- else if break ---else goto top
-                if (inputYN == "Y") {
+                if (studentInputYN == "Y") {
                     Console.WriteLine ("Enter students name in format 'last name , first name'.");
-                    studentNames = (Console.ReadLine ().ToUpper());
+                    studentName = (Console.ReadLine ().ToUpper ());
 
-                    Console.WriteLine ($"Enter grades for {studentNames}\nEach grade must be a number.\nYou may enter as many grades as you would like.");
-                    Console.WriteLine ($"To Exit entering grades for {studentNames}, type done.");
-                    //to loop and add grades to a list - exit when done;
-                
-                } else if (inputYN == "N") {
+                    Console.WriteLine ($"Enter grades for {studentName}.\nEach grade must be seperated by a coma.\nExample: (88,95)"); //interpolation is really cool!
+                    studentGrades = Console.ReadLine ();
+
+                    //enter string and gradestring into Gradebook
+                    gradeBook.Add (studentName, studentGrades);
+
+                    //else break loop
+                } else if (studentInputYN == "N") {
                     break;
 
+                    //else try again
                 } else {
                     Console.WriteLine ("You did not make a choice of 'Y' or 'N', try again");
                     goto Beginning;
                 }
-                    //I think I should test how the lists are taking in data with a foreach statement???
-                    //foreach ()
-                    //tried entering into dictionary here - didn't work threw exception ALSO
-                    //gradeBook.Add (studentNames, studentGrades);
             }
+            Console.WriteLine ("_________________________________________________________");
+            foreach (KeyValuePair <String, String> student in gradeBook) {
+                Console.WriteLine ($" Students: {student.Key} | Grades: {student.Value} ");
+            }
+            Console.WriteLine ("_________________________________________________________");
         }
-        //public static minMaxAvg(studentGrades) {
-            
-        //}
     }
 }

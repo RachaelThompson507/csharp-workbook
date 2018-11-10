2fb38cfe5b9b1b7aaf5bdca202b789420cfeb2cc
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-11-10 09:59:11 -0600

    working

--- Garage/Garage.cs
+++ Garage/Garage.cs
@@ -1,78 +1,69 @@
 ﻿using System;
 
+public class Program {
 
-public class Program
-{
-
-	public static void Main()
-	{
+    public static void Main () {
         // instantiating a new instance of car, and  passing "blue" to the constructor;
-		Car blueCar = new Car("blue");
-        Car goldCar = new Car("gold");
+        Car blueCar = new Car ("blue");
+        Car goldCar = new Car ("gold");
 
         // instantiating a new instaice of Garage class, and passing in 2 to the constructor;
-		Garage smallGarage = new Garage(2);
+        Garage smallGarage = new Garage (2);
 
         // calling a method Parkcar of the smallGarage instance, with inputs of blueCar and 0
-        try
-        {
-            smallGarage.ParkCar(blueCar, 0);
-            smallGarage.ParkCar(goldCar,1);
-        }
-        catch (System.ArgumentException e)
-        {
+        try {
+            smallGarage.ParkCar (blueCar, 0);
+            smallGarage.ParkCar (goldCar, 1);
+        } catch (System.ArgumentException e) {
             Console.WriteLine ("Exception Caught: {0}", e);
-        }
-        finally
-        {
+        } finally {
             // printint out the cars attribute of the small garage
-            Console.WriteLine(smallGarage.Cars);
+            Console.WriteLine (smallGarage.Cars);
         }
-	}
+    }
 }
 
-class Car
-{
+class Car {
     // constructor
     // takes in a strin that is the intial string
-    public Car(string initialColor)
-    {
+    public Car (string initialColor) {
         // setting the color of the car to be the string passed into the constructor
-    	Color = initialColor;
+        Color = initialColor;
     }
-    
+
     // changes the color of the car, even though it is a private attribute/variable
-    public void paintTheCar(String newColor){
+    public void paintTheCar (String newColor) {
         Color = newColor;
     }
 
     // once the color is set i cannt change it, outisde of this class
     public string Color { get; private set; }
 }
+class Person {
+    //constructor
+    public Person (String name){
+        
+    }
+}
 
-
-
-class Garage
-{
+class Garage {
     // an array of cars ? what does this do? why is it here?
-    
+
     private Car[] cars;
-    
+
     // constructor , int of initial size
-    public Garage(int initialSize)
-    {
+    public Garage (int initialSize) {
         // setting the size of this garage
-    	Size = initialSize;
+        Size = initialSize;
         // instantiating an array of Cars, of size initialsize
-	    this.cars = new Car[initialSize];
+        this.cars = new Car[initialSize];
     }
 
     //attribute , private for number of slots in the garage.
     public int Size { get; private set; }
-    
+
     // a method that adds a car to the spot in the cars array
-    public void ParkCar (Car car, int spot)
-    {
+    public void ParkCar (Car car, int spot) {
         if (cars[spot] == null) {
             cars[spot] = car;
         } else if (cars[spot] != null) {
@@ -80,20 +71,18 @@ class Garage
         } else {
             throw new System.ArgumentException ("You have made a choice that is outside of the garage.");
         }
-     }
-    
-    public string Cars {
-		get {
-            String carsString = "";
-			for (int i = 0; i < cars.Length; i++)
-			{
-				if (cars[i] != null) {
-					carsString += String.Format("The {0} car is in spot {1}.", cars[i].Color, i+1);
-                    carsString += "\n";
-				}
-			}
-			return carsString;
-		}
-	}
-}
+    }
 
+    public string Cars {
+        get {
+            String carsString = "";
+            for (int i = 0; i < cars.Length; i++) {
+                if (cars[i] != null) {
+                    carsString += String.Format ("The {0} car is in spot {1}.", cars[i].Color, i + 1);
+                    carsString += "\n";
+                }
+            }
+            return carsString;
+        }
+    }
+}

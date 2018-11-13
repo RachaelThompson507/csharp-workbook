fef0c641d19c5065241dd89ce4f112c6f2288e4d
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-11-12 20:17:32 -0600

    init commit

--- TowersOfHanoi/TowersOfHanoi.cs
+++ TowersOfHanoi/TowersOfHanoi.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
-namespace TowersOfHanoi
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace TowersOfHanoi {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Let's Play Tower of Hanoi");
         }
+    }
+    class Blocks {
+        public string block {get; private set;}
+        //do I need to specify the number of blocks
+        //public int numBlocks {get; private set;}
+        public Stack<string> ofBlocks {get; private set;}
+        public Blocks (string block, Stack<string> ofBlocks) {
+            this.block = block;
+            //how to create a stack of blocks
+            //this.ofBlocks = new ;
+        }
+    }
+    class Towers {
+        public string tower { get; private set;}
+        public Towers (string tower) {
+            this.tower = tower;
+        }
+    }
+    class Game {
+
+
     }
 }

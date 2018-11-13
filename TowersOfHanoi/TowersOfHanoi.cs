bcea1c1d737a7d58af0b3406d66b030062ede6fd
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-11-13 13:43:38 -0600

    work from last night

--- TowersOfHanoi/TowersOfHanoi.cs
+++ TowersOfHanoi/TowersOfHanoi.cs
@@ -8,13 +8,15 @@ namespace TowersOfHanoi {
             Console.WriteLine ("Let's Play Tower of Hanoi");
         }
     }
+    //blocks are numbers that are used to move between towers
     class Blocks {
         public int block {get; private set;}
-        
+    
         public Blocks (int block) {
             this.block = block;
         }
     }
+    //towers are "holders" of the blocks and keep them in order of largest - smallest LIFO
     class Towers {
         public string tower { get; private set;}
         public Stack<Blocks> fourBlocks {get; set;}
@@ -23,8 +25,9 @@ namespace TowersOfHanoi {
             this.fourBlocks = fourBlocks;
         }
     }
+    //has the user input and creation of the game
     class Game {
         public Dictionary<Towers, Stack<Blocks>> towersForGame {get; private set;}
-        
+
     }
 }

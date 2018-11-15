b17381e9a50f5e3b96e160df968a19b47b0c0982
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-11-14 18:01:13 -0600

    saving

--- TowersOfHanoi/TowersOfHanoi.cs
+++ TowersOfHanoi/TowersOfHanoi.cs
@@ -10,16 +10,16 @@ namespace TowersOfHanoi {
     }
     //blocks are numbers that are used to move between towers
     class Blocks {
-        public int block {get; private set;}
-    
+        public int block { get; private set; }
+
         public Blocks (int block) {
             this.block = block;
         }
     }
     //towers are "holders" of the blocks and keep them in order of largest - smallest LIFO
     class Towers {
-        public string tower { get; private set;}
-        public Stack<Blocks> fourBlocks {get; set;}
+        public string tower { get; private set; }
+        public Stack<Blocks> fourBlocks { get; set; }
         public Towers (string tower, Stack<Blocks> fourBlocks) {
             this.tower = tower;
             this.fourBlocks = fourBlocks;
@@ -27,7 +27,7 @@ namespace TowersOfHanoi {
     }
     //has the user input and creation of the game
     class Game {
-        public Dictionary<Towers, Stack<Blocks>> towersForGame {get; private set;}
+        public Dictionary<Towers, Stack<Blocks>> towersForGame { get; private set; }
 
     }
 }

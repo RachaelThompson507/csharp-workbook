85de7db211e97065bbe8bdf1e4c8a0cad603df64
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-11-14 20:09:18 -0600

    finished

--- Interfaces/Interface.cs
+++ Interfaces/Interface.cs
@@ -6,7 +6,50 @@ namespace Interfaces
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Console.WriteLine("Hello World!");
+            Plane x = new Plane ();
+            Console.WriteLine(x.Fly());
+            Console.WriteLine(x.Wings());
+            Bird y = new Bird ();
+            Console.WriteLine(y.Fly());
+            Console.WriteLine(y.Wings());
+            Pterodactyl p = new Pterodactyl ();
+            Console.WriteLine(p.Fly());
+            Console.WriteLine(p.Wings());
+
+        }
+    }
+    interface IThingsThatFly
+    {
+        string Fly();
+        string Wings();
+    }
+
+    class Plane : IThingsThatFly
+    {
+        public string Fly (){
+            return "Planes fly!";
+        }
+        public string Wings(){
+            return "Planes have wings!";
+        }
+    }
+    class Bird : IThingsThatFly
+    {
+       public string Fly (){
+            return "Birds  fly!";
+        }
+        public string Wings(){
+            return "Birds have wings!";
+        }
+    }
+    class Pterodactyl : IThingsThatFly
+    {
+        public string Fly (){
+            return "Pterodactyls fly!";
+        }
+        public string Wings(){
+            return "Pterodactyls have wings!";
         }
     }
 }

da12ac1e7116b6e46af87fe38bdeb0fcecd57511
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-11-14 20:54:36 -0600

    a little stuck

--- TowersOfHanoi/TowersOfHanoi.cs
+++ TowersOfHanoi/TowersOfHanoi.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 namespace TowersOfHanoi {
     class Program {
         static void Main (string[] args) {
+            //declare variables
+            //enter game
             Console.WriteLine ("Let's Play Tower of Hanoi");
         }
     }
@@ -19,15 +21,28 @@ namespace TowersOfHanoi {
     //towers are "holders" of the blocks and keep them in order of largest - smallest LIFO
     class Towers {
         public string tower { get; private set; }
-        public Stack<Blocks> fourBlocks { get; set; }
-        public Towers (string tower, Stack<Blocks> fourBlocks) {
+        Blocks block1 = new Blocks (4);
+        Blocks block2 = new Blocks (3);
+        Blocks block3 = new Blocks (2);
+        Blocks block4 = new Blocks (1);
+        public Stack<Blocks> StackBlocks { get; set; }
+        public Towers (string tower, Stack<Blocks> StackBlocks) {
             this.tower = tower;
-            this.fourBlocks = fourBlocks;
+            this.StackBlocks = StackBlocks;
+            StackBlocks.Push(block4);
+            StackBlocks.Push(block3);
+            StackBlocks.Push(block2);
+            StackBlocks.Push(block1);
         }
     }
     //has the user input and creation of the game
     class Game {
         public Dictionary<Towers, Stack<Blocks>> towersForGame { get; private set; }
+        //Towers towerA = new Towers ("A");
+        public Game (Dictionary <Towers, Stack<Blocks>> towersForGame) {
+            this.towersForGame = towersForGame;
+
+        }
 
     }
 }

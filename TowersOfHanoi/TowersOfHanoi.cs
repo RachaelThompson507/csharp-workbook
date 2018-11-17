39ef61629930180fe6d5a4a6b694f59ae8667076
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-11-16 18:36:32 -0600

    restart, needed to think through some issues

--- TowersOfHanoi/TowersOfHanoi.cs
+++ TowersOfHanoi/TowersOfHanoi.cs
@@ -5,45 +5,26 @@ using System.Collections.Generic;
 namespace TowersOfHanoi {
     class Program {
         static void Main (string[] args) {
-            //declare variables
-            Dictionary<Towers, Stack<Blocks>> towersForGame = new Dictionary <Towers, Stack<Blocks>> ();
             //enter game
             Console.WriteLine ("Let's Play Tower of Hanoi");
-            Game newgame = new Game (towersForGame);
         }
     }
-    //blocks are numbers that are used to move between towers
-    class Blocks {
-        public int block { get; private set; }
-
-        public Blocks (int block) {
-            this.block = block;
-        }
-    }
-    //towers are "holders" of the blocks and keep them in order of largest - smallest LIFO
-    class Towers {
-        public string tower { get; private set; }
-        
-        public Towers (string tower, Stack<Blocks> StackOfBlocks) {
-            this.tower = tower;
-        }
-        public void addBlocks() {
-            Blocks block1 = new Blocks (4);
-            Blocks block2 = new Blocks (3);
-            Blocks block3 = new Blocks (2);
-            Blocks block4 = new Blocks (1);
-            
-        }
-    }
-    //has the user input and creation of the game
-    class Game {
-        // public Dictionary<Towers, Stack<Blocks>> towersForGame { get; private set; }
-        //Make Towers- Add StackBlocks to Tower Keys in Dictionary
-        // Towers A = new Towers ("A", Stack<Blocks> StackBlocks);
-        public Game (Dictionary <Towers, Stack<Blocks>> towersForGame) {
-            this.towersForGame = towersForGame;
-
-        }
-
-    }
+    /*
+    Blocks are weighted by number, the greater number = more weight.
+    Blocks are interacted with by the user, moved between towers.
+    Blocks need to be linked to a tower. 4 needs to be the first block in so 1 can be moved first.
+     */
+    class Blocks { }
+    /*
+    Towers hold blocks, initially Tower A holds all blocks.
+    User then moves blocks between 3 towers.
+     */
+    class Towers { }
+    /*
+    Creates a game,
+    draws the board,
+    Allows the user to move blocks
+    Allows the user to choose towers.
+     */
+    class Game { }
 }

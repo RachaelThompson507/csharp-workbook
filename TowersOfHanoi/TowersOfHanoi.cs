9c36250863aa87936683effac4f325ac3f68cf7b
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-11-16 20:38:24 -0600

    trying to reorder how classes do things

--- TowersOfHanoi/TowersOfHanoi.cs
+++ TowersOfHanoi/TowersOfHanoi.cs
@@ -14,12 +14,31 @@ namespace TowersOfHanoi {
     Blocks are interacted with by the user, moved between towers.
     Blocks need to be linked to a tower. 4 needs to be the first block in so 1 can be moved first.
      */
-    class Blocks { }
+    class Blocks {
+        public int block {get; private set;}
+        public Blocks (int block) {
+            this.block = block;
+        }
+     }
     /*
     Towers hold blocks, initially Tower A holds all blocks.
     User then moves blocks between 3 towers.
      */
-    class Towers { }
+    class Towers {
+        public string tower {get; private set;}
+        public Stack<Blocks> towerBlocks {get; private set;}
+        public Towers (string tower, Stack<Blocks> towerBlocks) {
+            this.tower = tower;
+            this.towerBlocks= towerBlocks;
+        }
+        public void makeBlocks (Blocks block) {
+            Blocks four = new Blocks (4);
+            Blocks three = new Blocks (3);
+            Blocks two = new Blocks (2);
+            Blocks one = new Blocks (1);
+        }
+
+    }
     /*
     Creates a game,
     draws the board,

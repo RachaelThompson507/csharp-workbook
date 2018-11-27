a58343f13f6d2affcf4c5b61f561e7169c286318
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-11-26 19:19:47 -0600

    enums

--- week8-practice/Program.cs
+++ week8-practice/Program.cs
@@ -2,14 +2,30 @@
 
 namespace week8_practice
 {
+    enum PrimaryColors {
+        red, blue, yellow
+    }
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Enums!");
-            string junk = combineColors("hotPink", "tangerine");
+            string combined = combineColors("red", "yellow");
+            Console.WriteLine ("red + yellow = " +combined);
+
+            string primaryCombined = combinePrimaryColors(PrimaryColors.red, PrimaryColors.yellow);
+            Console.WriteLine ("red + yellow = " +primaryCombined);
         }
-        static String combineColors (String color1, String color2){
+        public static string combinePrimaryColors (PrimaryColors color1, PrimaryColors color2) {
+            if (color1 == PrimaryColors.red) {
+                if (color2 == PrimaryColors.yellow){
+                    return "orange";
+                } else if (color2 == PrimaryColors.blue){
+                    return "purple";
+                }
+            } return "unknown";
+        }
+        public static String combineColors (String color1, String color2){
             if (color1 == "red") {
                 if (color2 == "yellow"){
                     return "orange";

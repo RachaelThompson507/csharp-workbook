ab2236bbc99083e0bc0a71bcffc12879f0c1d248
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-11-26 19:08:07 -0600

    new practice file

---
+++ week8-practice/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace week8_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Enums!");
+            string junk = combineColors("hotPink", "tangerine");
+        }
+        static String combineColors (String color1, String color2){
+            if (color1 == "red") {
+                if (color2 == "yellow"){
+                    return "orange";
+                } else if (color2 == "blue"){
+                    return "purple";
+                }
+            } return "unknown";
+        }
+    }
+}

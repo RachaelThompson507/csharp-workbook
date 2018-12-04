626dc726076c6c8a52fe11250cb69c08c80456e8
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-12-03 19:29:36 -0600

    File reading

---
+++ week9-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+// file sharing / using files
+namespace week9_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            String file = @"test.txt";
+            String [] lines = File.ReadAllLines(file);
+
+            int lineNo = 0;
+            foreach (string line in lines) {
+                lineNo +=1;
+                Console.WriteLine (lineNo +"- " +line);
+            }
+        }
+    }
+}

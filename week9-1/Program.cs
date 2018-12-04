22516fd355502aafb49b702c8b2f032f0955cd4b
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-12-03 20:15:40 -0600

    YS to send examples

--- week9-1/Program.cs
+++ week9-1/Program.cs
@@ -9,13 +9,26 @@ namespace week9_1
         {
             Console.WriteLine("Hello World!");
             String file = @"test.txt";
+            //read all the lines in the file
             String [] lines = File.ReadAllLines(file);
 
+            //read lines are printed to the console
             int lineNo = 0;
             foreach (string line in lines) {
                 lineNo +=1;
                 Console.WriteLine (lineNo +"- " +line);
             }
+
+            //write to the file
+            String copyFile = @"test1.txt";
+            File.AppendAllLines (copyFile, lines);
+            //print the new one
+            // int lineNo1 = 0;
+            // String [] lines1 = File.ReadAllLines(copyFile);
+            // foreach (string line1 in lines1) {
+            //     lineNo1 +=1;
+            //     Console.WriteLine (lineNo1 +"- " +lines1);
+            // }
         }
     }
 }
